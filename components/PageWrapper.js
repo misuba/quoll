5f612f4c8210b0613bb2ff2c4cae74129c7ebfe5
mike sugarbaker <misuba@gibberish.com>
2017-01-15 11:36:18 -0800

    Apply some bootstrappery

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -5,10 +5,10 @@ const {connect} = require('react-redux');
 
 
 const PageWrapper = function(props) {
-    return <div className="top">
-        <header>
+    return <div className="top container">
+        <header className="row">
             {props.user && Object.keys(props.user).length
-                ? <nav className="user">
+                ? <nav className="user col">
                     {props.user.username}&nbsp;
                     <span className="logout">(<a href="/logout">Logout</a>)</span>
                     <hr />
@@ -17,9 +17,11 @@ const PageWrapper = function(props) {
             }
             {props.flash && <aside className="error">{props.flash.error}</aside>}
         </header>
-        {props.nav && <nav>{props.nav}</nav>}
-        {props.children}
-        <footer>
+        <section className="row">
+          {props.nav && <nav className="col-md-3">{props.nav}</nav>}
+          {props.children}
+        </section>
+        <footer className="row">
             FOOTER
         </footer>
     </div>;
